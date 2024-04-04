95d495819f41ea3ce2e894b9f0922b3c4aec381e
Diogo Antonny <Diogojp202@gmail.com>
2024-04-04 12:18:55 -0300

    Estrutura de arquivos organizada, imagens adicionadas, fontes adicionadas, estilização mobile iniciada e script javascript criado e desenvolvido

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let ativado = false;
+
+document.addEventListener("click", event => {
+    const element = event.target;
+
+    if(element.classList.contains("hamburger") || element.classList.contains("line")){
+        const hamburger = document.querySelectorAll(".line");
+        const menu =  document.querySelectorAll(".links")[0];
+        
+        if(ativado === false){
+            hamburger[0].style.transform = "rotate(45deg)";
+            hamburger[0].style.top = "43%";
+            hamburger[1].style.height = "0%";
+            hamburger[2].style.transform = "rotate(-45deg)";
+            hamburger[2].style.top = "-43%";
+
+            menu.style.display = "flex"
+
+            return ativado = true;
+        };
+
+        if(ativado === true){
+            hamburger[0].style.transform = "rotate(0deg)"
+            hamburger[0].style.top = "0";
+            hamburger[1].style.height = "15%";
+            hamburger[2].style.transform = "rotate(0deg)";
+            hamburger[2].style.top = "0";
+
+            menu.style.display = "none"
+
+            return ativado = false;
+        };
+    };
+});
